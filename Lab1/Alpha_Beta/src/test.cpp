0134f5b877b6dc5789a519d037fe44ba15c16fd6
Melmaphother <1559792558@qq.com>
2024-04-20 00:12:50 +0800

    modify src frame

--- Lab1/Alpha_Beta/src/test.cpp
+++ Lab1/Alpha_Beta/src/test.cpp
@@ -1,13 +1,14 @@
-#include "node.h"
 #include <fstream>
+#include "node.h"
 
 using namespace ChineseChess;
 
-// 博弈树搜索，depth为搜索深度
-int alphaBeta(GameTreeNode node, int alpha, int beta, int depth,
-              bool isMaximizer) {
-    if (depth == 0) { return node.getEvaluationScore(); }
-    // TODO alpha-beta剪枝过程
+//博弈树搜索，depth为搜索深度
+int alphaBeta(GameTreeNode node, int alpha, int beta, int depth, bool isMaximizer) {
+    if (depth == 0) {
+        return node.getEvaluationScore();
+    }
+    //TODO alpha-beta剪枝过程
     return 0;
 }
 
@@ -20,7 +21,9 @@ int main() {
     while (std::getline(file, line)) {
         std::vector<char> row;
 
-        for (char ch: line) { row.push_back(ch); }
+        for (char ch : line) {
+            row.push_back(ch);
+        }
 
         board.push_back(row);
         n = n + 1;
@@ -30,10 +33,11 @@ int main() {
     file.close();
     GameTreeNode root(true, board, std::numeric_limits<int>::min());
 
-    // TODO
-    //  alphaBeta(root, )
+    //TODO
+    // alphaBeta(root, )
 
-    // 代码测试
+
+    //代码测试
     ChessBoard _board = root.getBoardClass();
     std::vector<std::vector<char>> cur_board = _board.getBoard();
 
@@ -48,17 +52,13 @@ int main() {
     std::vector<Move> black_moves = _board.getMoves(false);
 
     for (int i = 0; i < red_moves.size(); i++) {
-        std::cout << "init: " << red_moves[i].init_x << " "
-                  << red_moves[i].init_y << std::endl;
-        std::cout << "next: " << red_moves[i].next_x << " "
-                  << red_moves[i].next_y << std::endl;
+        std::cout << "init: " << red_moves[i].init_x <<  " " << red_moves[i].init_y << std::endl;
+        std::cout << "next: " << red_moves[i].next_x <<  " " << red_moves[i].next_y << std::endl;
         std::cout << "score " << red_moves[i].score << std::endl;
     }
     for (int i = 0; i < black_moves.size(); i++) {
-        std::cout << "init: " << black_moves[i].init_x << " "
-                  << black_moves[i].init_y << std::endl;
-        std::cout << "next: " << black_moves[i].next_x << " "
-                  << black_moves[i].next_y << std::endl;
+        std::cout << "init: " << black_moves[i].init_x <<  " " << black_moves[i].init_y << std::endl;
+        std::cout << "next: " << black_moves[i].next_x <<  " " << black_moves[i].next_y << std::endl;
         std::cout << "score " << black_moves[i].score << std::endl;
     }
 
